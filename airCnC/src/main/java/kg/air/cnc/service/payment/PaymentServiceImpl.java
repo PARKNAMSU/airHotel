78e07cd84dde3d455d7598ca7b927e71eef20dc7
jaden <jonghaha7@gmail.com>
2020-08-11 13:08:32 +0900

    결제페이지

--- airCnC/src/main/java/kg/air/cnc/service/payment/PaymentServiceImpl.java
+++ airCnC/src/main/java/kg/air/cnc/service/payment/PaymentServiceImpl.java
@@ -2,6 +2,27 @@ package kg.air.cnc.service.payment;
 
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Service("PaymentService")
 public class PaymentServiceImpl implements PaymentService {
+
+    public int calculatePay(String checkin, String checkout){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try{
+            Date checkIn = format.parse(checkin);
+            Date checkOut = format.parse(checkout);
+
+            long calDate = checkIn.getTime() - checkOut.getTime();
+            long calDateDay = calDate / (24*60*60*1000);
+            calDateDay = Math.abs(calDateDay);
+            return (int)calDateDay;
+
+        } catch (ParseException e){
+            e.printStackTrace();
+            return -1;
+        }
+    }
 }
